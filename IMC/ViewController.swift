3d3c2dd21cb2992e6598ad0ff13ffc5f253cb434
user140090 <user140090@telehouse-moboware.dmarc.lga11.atlanticmetro.net>
2018-08-20 20:35:31 -0400

    Calculo IMC

--- IMC/ViewController.swift
+++ IMC/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var tfWeight: UITextField!
     @IBOutlet weak var tfHeight: UITextField!
+    @IBOutlet weak var viResult: UIView!
     @IBOutlet weak var lbResult: UILabel!
     @IBOutlet weak var ivResult: UIImageView!
     
@@ -27,11 +28,42 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+    }
     @IBAction func calculate(_ sender: Any) {
         if let weight = Double(tfWeight.text!), let height = Double(tfHeight.text!) {
-            
+            imc = weight / (height * height)
+            showResults()
         }
     }
     
+    func showResults() {
+        var result: String = ""
+        var image: String = ""
+        
+        switch imc {
+            case 0..<16:
+                result = "Magreza"
+                image = "abaixo"
+            case 16..<18.5:
+                result = "Abaixo do peso"
+                image = "abaixo"
+            case 18.5..<25:
+                result = "Peso ideal"
+                image = "ideal"
+            case 25..<30:
+                result = "Sobrepeso"
+                image = "sobre"
+            default:
+                result = "Obesidade"
+                image = "obesidade"
+        }
+        
+        lbResult.text = "\(Int(imc)): \(result)"
+        ivResult.image = UIImage(named: image)
+        viResult.isHidden = false
+        view.endEditing(true)        
+    }
 }
 
